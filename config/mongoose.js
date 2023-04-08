f80176ae4d0e2319ffb924137fd20917d504c5bd
amanpatel23 <ap289503@gmail.com>
2023-04-09 02:39:47 +0530

    modified mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -9,7 +9,10 @@ const password = process.env.DB_PASSWORD;
 
 // Connect to a MongoDB database at the given URL using the Mongoose connect method
 const address = `mongodb+srv://${username}:${password}@cluster0.wthkym3.mongodb.net/?retryWrites=true&w=majority`
-mongoose.connect(address).then(() => {
+mongoose.connect(address, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
     console.log('Connected to MongoDB');
 }).catch((error) => {
     console.log(`Error connecting to MongoDB: ${error}`);
