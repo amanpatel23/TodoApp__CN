38f1e36d3334120a0ad610054f8ffc8cbeb7e801
amanpatel23 <ap289503@gmail.com>
2023-04-09 01:59:39 +0530

    secured username and password

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,14 @@
 
+require('dotenv').config();
+
 // Import the Mongoose module and connect to a MongoDB database
 const mongoose = require('mongoose');
 
+const username = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+
 // Connect to a MongoDB database at the given URL using the Mongoose connect method
-const address = 'mongodb+srv://amanpatel23:amanpatel@cluster0.wthkym3.mongodb.net/?retryWrites=true&w=majority'
+const address = `mongodb+srv://${username}:${password}@cluster0.wthkym3.mongodb.net/?retryWrites=true&w=majority`
 mongoose.connect(address).then(() => {
     console.log('Connected to MongoDB');
 }).catch((error) => {
